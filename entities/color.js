7f300930508a9af6e35ca3101ad316cba2a65706
sidneyaguirre <sidney.aguirrec@gmail.com>
2018-11-10 13:45:51 -0500

    changes on front

---
+++ entities/color.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose') // mongoose es algo que nos permite facilidad a la hora de manejar esquemas y hacer consultar
+let schema = mongoose.Schema // se defina una variable Schema
+
+let colorSchemaJSON = {
+    id: {
+        type: String,
+    }
+}
+
+
+let colorSchema = new schema(colorSchemaJSON) // Creacion del esquema como tal
+let color = mongoose.model('Color', colorSchema) // creacion del modelo, este es que conecta con la bd, se le pasa el esquema de la tabla a//Creacion del esquema como tal
+
+module.exports = { // Exporta todos los metodos
+    color
+}
